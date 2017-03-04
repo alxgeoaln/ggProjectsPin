3dc70e25c4dfbe63e074189d820390f70b071693
alxgeoaln <georgianalinalexandru@gmail.com>
2017-03-04 17:48:33 +0200

    modified model/pin

--- model/pin.js
+++ model/pin.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var PinSchema   = new Schema({
-    userID : String,
+    authorId : String,
     latitude : String,
     longitude : String
 });
